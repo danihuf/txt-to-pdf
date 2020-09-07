4de51f17f22e890ea0ff0e81352b7ee7dc80b6a5
hufnagelda81215 <hufnagelda81215@th-nuernberg.de>
2020-09-07 14:49:14 +0200

    c# project initialized

---
+++ pdfCreator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pdfCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
