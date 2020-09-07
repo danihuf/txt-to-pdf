8a3e4cfc27d40dec558c53ec83043be6861883ae
danihuf <daniel99h@gmail.com>
2020-09-07 16:07:11 +0200

    Started implementing generatePdf function.
    Apparently drawing characters doesn't work on linux with pdfsharp?
    todo: try on windows machine

--- pdfCreator/Program.cs
+++ pdfCreator/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.IO;
 using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PresentationCore;
 
 
 namespace pdfCreator
@@ -8,7 +12,12 @@ namespace pdfCreator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Run();
+        }
+
+        static void Run()
+        {
+            generatePdf(getLocation(), "test");
         }
 
         /// <summary>
@@ -22,6 +31,59 @@ namespace pdfCreator
             return location;
         }
 
-        //test
+
+        static void generatePdf(string location, string name)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(location))
+                {
+                    string line;
+                    int lineCounter = 0;
+                    int lineThreshold = 50;
+
+                    PdfDocument pdf = new PdfDocument();
+                    pdf.Info.Title = name;
+                    // add first page to pdf document
+                    PdfPage page = pdf.AddPage();
+                    XGraphics gfx = XGraphics.FromPdfPage(page);
+                    XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
+
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        //iterate over all lines in document
+                        lineCounter++;
+
+                        gfx.DrawString(line, font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height));
+
+
+                        if (lineCounter > lineThreshold)
+                        {
+                            // create new page
+
+                        }
+                    }
+
+                    pdf.Save(location);
+                    
+
+
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                // start program again when false input was made
+                System.Console.WriteLine(e.Message);
+                System.Console.WriteLine("Please enter a valid file location");
+                Run();
+
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+        }
+
     }
 }
