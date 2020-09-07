b496fb8adb235578e6dd23928be4e8800fb6382b
hufnagelda81215 <hufnagelda81215@th-nuernberg.de>
2020-09-07 15:05:45 +0200

    Implemented getLocation function.
    Promts the user to enter files location to generate pdf from it.

--- pdfCreator/Program.cs
+++ pdfCreator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using PdfSharp;
+
 
 namespace pdfCreator
 {
@@ -8,5 +10,17 @@ namespace pdfCreator
         {
             Console.WriteLine("Hello World!");
         }
+
+        /// <summary>
+        /// Prompts the user to enter the files location
+        /// </summary>
+        /// <returns> Location of the file as string </returns>
+        public static string getLocation()
+        {
+            System.Console.WriteLine("Welcome to pdfCreator. \n Please enter the location of the .txt file.");
+            string location = Console.ReadLine();
+            return location;
+        }
+
     }
 }
